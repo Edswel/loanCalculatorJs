18b240866392422a3d947527ee4105e91bc98679
Edswel <franckini@yahoo.com>
2022-10-17 15:21:57 +0100

    Populate loan calculated results

--- app.js
+++ app.js
@@ -14,5 +14,16 @@ function calculateResults(e) {
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
 
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    console.log('Error man');
+  }
+
   e.preventDefault();
 }
