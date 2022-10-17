ff1314f024cabbf18a6a2a471f3ec96d80ded114
Edswel <franckini@yahoo.com>
2022-10-17 15:08:16 +0100

    Select elements

---
+++ app.js
@@ -0,0 +1,14 @@
+// Listen for submit event
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate result
+function calculateResults(e) {
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  e.preventDefault();
+}
