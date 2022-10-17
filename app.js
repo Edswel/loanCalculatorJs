1e76982cd48606b1cd919c393621d949b6ed5a40
Edswel <franckini@yahoo.com>
2022-10-17 15:55:32 +0100

    Add loading spinner

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // Listen for submit event
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate result
-function calculateResults(e) {
+function calculateResults() {
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
@@ -21,14 +31,24 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide spinner
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('An error occurred. Kindly check your numbers and try again.');
   }
-
-  e.preventDefault();
 }
 
 function showError(error) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Hide spinner
+  document.getElementById('loading').style.display = 'none';
+
   // Create div
   const errorBox = document.createElement('div');
 
@@ -44,4 +64,12 @@ function showError(error) {
 
   // Insert error message
   card.insertBefore(errorBox, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+// Clear error function
+function clearError() {
+  document.querySelector('.alert').remove();
 }
