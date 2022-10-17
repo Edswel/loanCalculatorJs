1cfb784cd325c9fdc2f46b0bc620420594cfbc94
Edswel <franckini@yahoo.com>
2022-10-17 15:13:10 +0100

    Calculate interest and payments

--- app.js
+++ app.js
@@ -10,5 +10,9 @@ function calculateResults(e) {
   const totalPayment = document.getElementById('total-payment');
   const totalInterest = document.getElementById('total-interest');
 
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
   e.preventDefault();
 }
